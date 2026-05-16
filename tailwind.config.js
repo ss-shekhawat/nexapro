bb53044e27aad050d11bb3a6a5ae3ec57f59440e
ss-shekhawat <ssshekhawat0896@gmail.com>
2026-05-16 11:02:07 +0530

    Change primary and secondary colors in config
    Updated color palette in Tailwind configuration.

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,16 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: '#2196F3',      // Sky Professional 
-        secondary: '#2B2D42',    // Dark navy
-        accent: '#8D99AE',       // Light gray-blue
-        light: '#EDF2F4',        // Off-white
-        dark: '#414141',         // Dark gray for text
+        // primary: '#2196F3',      // Sky Professional 
+        // secondary: '#2B2D42',    // Dark navy
+        // accent: '#8D99AE',       // Light gray-blue
+        // light: '#EDF2F4',        // Off-white
+        // dark: '#414141',         // Dark gray for text
+        primary: "#1976D2",
+        secondary: "#1565C0",
+        dark: "#1E1E1E",
+        light: "#FFFFFF",
+        accent: "#EDF2F4",
       },
       fontFamily: {
         sans: ['Montserrat', 'sans-serif'],
